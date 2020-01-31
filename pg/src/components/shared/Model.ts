249a7cf1b05ee8aa9425d9d6b7f16a7744163f42
sen li <sen.li@sinefa.com>
2020-01-31 14:11:28 +1100

    pg: Fix shared Model unit test warning

--- pg/src/components/shared/Model.ts
+++ pg/src/components/shared/Model.ts
@@ -1,6 +1,7 @@
 import * as Joi from "@hapi/joi";
 
 export default class Model {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public static fromJson(json: any, schema: Joi.ObjectSchema): Model {
     Joi.assert(json, schema);
     return Object.assign(new Model(), json);
